3ccad4a218cc229cf3ded7c7328b9c799dc7a75a
Leona_Lenovo <Leona_Lenovo@Leona_Lenovo-PC>
2015-01-07 16:28:34 -0200

    metodo Capturar  imagem

---
+++ src/main/java/br/leona/hardware/camera/CameraController.java
@@ -0,0 +1,51 @@
+package br.leona.hardware.camera;
+
+import java.io.IOException;
+import javax.media.CannotRealizeException;
+import javax.media.Manager;
+import javax.media.MediaLocator;
+import javax.media.NoPlayerException;
+import javax.media.Player;
+
+/**
+ *
+ * @author Leona_Lenovo
+ */
+public final class CameraController {
+
+    Player player;
+    Capturar capturar;
+
+  
+
+    public void iniciarCamera() throws IOException, NoPlayerException, CannotRealizeException {
+
+        MediaLocator m1 = new MediaLocator("vfw://0");
+        
+         // para pensar!!!
+      /*  transmissor = new Transmitter(new MediaLocator("vfw://0"),
+                                              hostAddress, 1235, 
+                                              new Format(VideoFormat.JPEG));*/
+        
+        
+        player = Manager.createRealizedPlayer(m1);
+        //  this.tbcamera.add(player.getVisualComponent());
+        player.start();
+        System.out.println("");
+
+    }
+
+    public String capturarCamera(String capture) {
+
+        if (capture.equals("on")) {
+            System.out.println("**************Inicia de capturar imagens**********");
+            capturar = new Capturar(this);
+            capturar.start();
+        } else {
+            capturar.stop();
+            System.out.println("**************Cancela de capturar imagens**********");
+        }
+        return capture;
+    }
+   } 
+
---
+++ src/main/java/br/leona/hardware/camera/Capturar.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.leona.hardware.camera;
+
+import java.awt.FileDialog;
+import java.awt.Frame;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import javax.imageio.ImageIO;
+import javax.media.Buffer;
+import javax.media.control.FrameGrabbingControl;
+import javax.media.format.VideoFormat;
+import javax.media.util.BufferToImage;
+
+/**
+ *
+ * @author Leona_Lenovo
+ */
+public class Capturar extends Thread {
+
+    private final CameraController cameraController;
+    String nfile;
+    BufferedImage bi;
+    LocalDateTime agora;
+
+    Capturar(CameraController cameraController) {
+
+        this.cameraController = cameraController;
+    }
+
+    @Override
+    public void run() {
+
+        Date date = new Date(System.currentTimeMillis());
+        SimpleDateFormat format2 = new SimpleDateFormat("hh mm ss");
+        String formatada = format2.format(date);
+
+        String diretorio = "C:/ProjetoLeona/";
+        String observacao = "Evento "
+                + "" + localDate().format(DateTimeFormatter.ofPattern("ddMMyyyy"))
+                + " " + formatada
+                + "/";
+        String imagem;
+
+        int[] a = new int[1080000];
+        File dir = new File(diretorio + observacao);
+
+        FrameGrabbingControl fgc = (FrameGrabbingControl) cameraController.player.getControl("javax.media.control.FrameGrabbingControl");
+
+        Buffer buffer = fgc.grabFrame();
+
+        BufferToImage bti = new BufferToImage((VideoFormat) buffer.getFormat());
+        Image image = bti.createImage(buffer);
+        bi = (BufferedImage) image;
+
+        if (dir.mkdirs()) {
+
+            for (int i = 0; i < a.length; i++) {
+
+                //agora = LocalDateTime.now();
+                Date date1 = new Date(System.currentTimeMillis());
+                SimpleDateFormat hora = new SimpleDateFormat("HH mm ss");
+
+                FileDialog fd = new FileDialog(new Frame(), " ", FileDialog.SAVE);
+
+                imagem = "[Evento" + i + "]" + localDate() + " " + hora.format(date1);
+                System.out.println("Evento " + i + "]" + localDate() + " " + hora.format(date1));
+                fd.setDirectory(diretorio + observacao);
+
+                fd.setFile(imagem);
+
+                if (fd.getFile() != null) {
+                    nfile = fd.getDirectory() + fd.getFile() + ".jpg";
+                    try {
+
+                        File make = new File(nfile);
+                        try {
+
+                            ImageIO.write(bi, "jpg", make);
+                        } catch (IOException ex) {
+
+                        }
+                    } catch (Exception ex) {
+                    }
+                }
+            }
+        } else {
+
+            System.out.println("Nao foi possivel criar o diretorio");
+        }
+       
+    }
+
+    private LocalDate localDate() {
+
+        LocalDate hoje = LocalDate.now();
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd MM yyyy");
+        hoje.format(formatador); // exemplo 08/04/2014
+        return hoje;
+    }
+
+}
